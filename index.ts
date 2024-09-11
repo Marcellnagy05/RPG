cf1636d3f130e6cc236234b60c3fbfbe7518d099
Marcellnagy05 <marcika863@gmail.com>
2024-09-11 10:09:59 +0200

    Added Driver object

--- index.ts
+++ index.ts
@@ -1,4 +1,25 @@
 let username: string = "Nagy Marcell";
 let age: number = 19;
-let gay: boolean = false;
+let driverLicense: boolean = true;
+
+let data: Object = {
+    username: "Kumpri",
+    age: 21
+}
+
+type Driver = {
+    name: string,
+    age: number,
+    validDriverLicense: boolean,
+    blackMarks: number,
+    countryCode: 'H' | 'GB' | 'CH' | 'I'
+}
+
+let Marci: Driver = {
+    name: "Nagy Marcell",
+    age: 19,
+    validDriverLicense: true,
+    blackMarks: 0,
+    countryCode: 'H'
+}
 
