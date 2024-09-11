7a58f385cc42f5f0d3e152efe809c57e6750eb0d
Marcellnagy05 <marcika863@gmail.com>
2024-09-11 13:37:54 +0200

    stats type added

--- sample.js
+++ sample.js
@@ -1,4 +1,6 @@
 var API_URL = " https://pokeapi.co/api/v2/pokemon/pikachu";
 fetch(API_URL).then(function (res) { return res.json(); }).then(function (data) {
-    console.log("".concat(data.name, ": Magass\u00E1ga: ").concat(data.height, ", Exp: ").concat(data.base_experience, "xp, Forms: ").concat(data.forms.name, " / ").concat(data.forms.url));
+    console.log("".concat(data.name, ": Magass\u00E1ga: ").concat(data.height, ", \n        Exp: ").concat(data.base_experience, "xp, \n        ").concat(data.stats.forEach(function (element) {
+        console.log(element.base_stat);
+    })));
 });
--- sample.ts
+++ sample.ts
@@ -3,11 +3,18 @@ const API_URL = " https://pokeapi.co/api/v2/pokemon/pikachu";
 type Pokemon = {
     id: number,
     name: string,
+    stats: Stat[],
     base_experience: number,
     height: number,
-    forms: Forms
-    ,
+    forms: Forms,
     is_default: boolean
+}
+
+type Stat ={
+    base_stat: number,
+    stat:{
+        name: string
+    }
 }
 
 type Forms = {
@@ -16,6 +23,11 @@ type Forms = {
 }
 
 fetch(API_URL).then(res => res.json()).then((data: Pokemon) => {
-    console.log(`${data.name}: Magassága: ${data.height}, Exp: ${data.base_experience}xp, Forms: ${data.forms.name} / ${data.forms.url}`);
+    console.log(`${data.name}: Magassága: ${data.height}, 
+        Exp: ${data.base_experience}xp, 
+        ${data.stats.forEach(element => {
+        console.log(element.base_stat);
+        
+    })}`);
     
 })
