98d75e8d9cdd51bab696cbcd312090c62ebd17b7
Marcellnagy05 <marcika863@gmail.com>
2024-09-11 12:58:21 +0200

    test

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ let data: Object = {
     age: 21
 }
 
-type Driver = {
+interface IDriver {
     name: string,
     age: number,
     validDriverLicense: boolean,
@@ -15,11 +15,32 @@ type Driver = {
     countryCode: 'H' | 'GB' | 'CH' | 'I'
 }
 
-let Marci: Driver = {
+interface IDriver {
+    bloodAlcohol: number
+}
+
+let Marci: IDriver = {
     name: "Nagy Marcell",
     age: 19,
     validDriverLicense: true,
     blackMarks: 0,
-    countryCode: 'H'
+    countryCode: 'H',
+    bloodAlcohol: 0
 }
 
+type Car = {
+    carType: string,
+    isRollin: boolean,
+}
+
+type Airplane ={
+    airPlaneNumber: number,
+    isFlying: boolean
+}
+
+type Vehicle = Car | Airplane
+
+let mustang: Vehicle = {
+    carType: "muscle",
+    isRollin: true
+}
